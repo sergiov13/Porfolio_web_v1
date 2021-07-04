cb0da795049f92dd51ae17155ab7e6c76d10f7c5
SergioV <sergiovillap93@gmail.com>
2021-07-04 02:43:01 -0300

    "Comments"

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import App from '../components/App';
 import './index.css'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
+ /* test */ 
+ /* test */
 
 const index = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={App} exact />
+        <Route path="/index2" component={App} exact />
       </Switch>
     </Router>
   );
