f8753ea51a79ec215fd70465535636ac5f64d7e5
SergioV <sergiovillap93@gmail.com>
2021-07-06 19:59:10 -0300

    "More fixes for netlify"

--- .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -6,7 +6,6 @@ const preferDefault = m => (m && m.default) || m
 exports.components = {
   "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\PC\\Desktop\\Git\\GIT\\Personal_Web\\.cache\\dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("C:\\Users\\PC\\Desktop\\Git\\GIT\\Personal_Web\\src\\pages\\404.js")),
-  "component---src-pages-index-2-js": preferDefault(require("C:\\Users\\PC\\Desktop\\Git\\GIT\\Personal_Web\\src\\pages\\index2.js")),
   "component---src-pages-index-js": preferDefault(require("C:\\Users\\PC\\Desktop\\Git\\GIT\\Personal_Web\\src\\pages\\index.js"))
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import App from '../components/App';
 import './index.css'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
 
 const index = () => {
   return (
-    <Router>
-      <Switch>
-        <Route component={App} exact />
-      </Switch>
-    </Router>
+   <App />
   );
 }
 
